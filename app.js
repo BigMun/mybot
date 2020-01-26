0c08ec06101a5abded9db1c2e7a8f7b4a040e3a6
BigMun <stephenhav921@gmail.com>
2020-01-26 01:36:29 +0000

    Added more fields to db collection

--- app.js
+++ app.js
@@ -11,15 +11,25 @@ const db = require('./db')
 command.on('message', message =>{
     
     const content = message;
-    const member = message.member.user.tag
-    const isbot = message.member.user.bot
-    const channel = message.channel.name
-     
+    const tag = message.member.user.tag;
+    const isbot = message.member.user.bot;
+    const channel = message.channel.name;
+    const userid = message.member.user.id;
+    const username = message.member.user.username;
+    const nickname = message.member.nickname;
+    const discriminator = message.member.user.discriminator
+    const avatarURL = message.member.user.avatarURL
+
     const data = {
         content: content,
-        member: member,
+        tag: tag,
         isbot: isbot,
-        channel: channel
+        channel: channel,
+        userid: userid,
+        username: username,
+        nickname: nickname,
+        discriminator: discriminator,
+        avatarURL: avatarURL
     }
     
     db.query('INSERT INTO messages SET ?', [data], (err, rows, fields) =>{
